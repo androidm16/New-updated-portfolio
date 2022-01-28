74f6b4334095a860b532fb985a5ac66fba727afe
lifechoices <mkhosanasiyabonga@gmail.com>
2022-01-28 10:20:50 +0200

    first commit

---
+++ views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+  <h1> Siyabonga Mkhosana </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ views/Testimonial.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="Testimonial">
+       <h1>Here's the fans</h1>
+</template>
+
+<script>
+export default {
+ name: 'testimonial',
+}
+</script>
+
+<style>
+
+</style>
